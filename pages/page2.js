ee39d977477b144e2206f5062afb5370ea5427ad
Andrew Lee <alee14498@protonmail.com>
2021-04-27 14:45:25 +0000

    added more content

--- pages/page2.js
+++ pages/page2.js
@@ -11,11 +11,8 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1>The War of 1812</h1>
-        <h3>By: Andrew Lee</h3>
-        <img src="https://www.history.com/.image/ar_16:9%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cg_faces:center%2Cq_auto:good%2Cw_620/MTU3ODc3NjU2MjE2MzQ4Mzgz/battle-of-new-orleans.jpg" width="80%" height="80%" />
-        <br/>
-        <Button variant="contained" color="primary" href="/page2">Next Page</Button>
+        <h1>The Cause Of This War</h1>
+        <Button variant="contained" color="primary" href="#">Next Page</Button>
       </main>
     </div>
   )
